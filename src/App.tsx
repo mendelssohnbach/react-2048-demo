2234e896bd440c778eadb37660c9e4a44c9ba5eb
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-06-22 15:04:05 +0900

    chapter03 コンポーネントを試してみよう

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+interface TileProps {
+  value: number;
+}
+
+function Tile({ value }: TileProps) {
+  return <div style={{ border: '1px solid black', padding: '10px', margin: '5px' }}>{value}</div>;
+}
+
 export default function App() {
   return (
     <div>
       <h1>2048</h1>
+      <Tile value={2} />
+      <Tile value={4} />
+      <Tile value={8} />
     </div>
   );
 }
