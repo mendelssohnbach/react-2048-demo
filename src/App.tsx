cc9369325d7da03c581a39045f4f8f9f56998857
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-06-23 07:30:10 +0900

    chapter9 キーボードイベントを登録する

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 import { createInitialBoard } from './utils/gameLogic';
@@ -6,6 +6,32 @@ import { createInitialBoard } from './utils/gameLogic';
 function App() {
   const [board, setBoard] = useState(() => createInitialBoard());
   const [score, setScore] = useState<number>(0);
+
+  // TODO
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case 'ArrowLeft':
+          console.log('左');
+          break;
+        case 'ArrowRight':
+          console.log('右');
+          break;
+        case 'ArrowUp':
+          console.log('上');
+          break;
+        case 'ArrowDown':
+          console.log('下');
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   const handleRestart = () => {
     setBoard(createInitialBoard());
