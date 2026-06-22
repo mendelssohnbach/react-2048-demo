9cf81c9f4bda8d9cc391c0000ef4baae0c289915
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-06-23 08:15:29 +0900

    chapter10 App.tsxに移動処理をつなぐ

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,30 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Board from './components/Board';
-import { createInitialBoard } from './utils/gameLogic';
+import { addRandomTile, createInitialBoard, moveLeft, moveRight } from './utils/gameLogic';
 
 function App() {
   const [board, setBoard] = useState(() => createInitialBoard());
   const [score, setScore] = useState<number>(0);
 
-  // TODO
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      switch (e.key) {
-        case 'ArrowLeft':
-          console.log('左');
-          break;
-        case 'ArrowRight':
-          console.log('右');
-          break;
-        case 'ArrowUp':
-          console.log('上');
-          break;
-        case 'ArrowDown':
-          console.log('下');
-          break;
-      }
+      let result = null;
+
+      if (e.key === 'ArrowLeft') result = moveLeft(board);
+      if (e.key === 'ArrowRight') result = moveRight(board);
+
+      if (!result) return;
+
+      e.preventDefault();
+      const newBoard = addRandomTile(result.board);
+      setBoard(newBoard);
+      setScore((prev) => prev + result.gained);
     };
 
     window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [board]);
 
   const handleRestart = () => {
     setBoard(createInitialBoard());
